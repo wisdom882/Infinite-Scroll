55b70816d6bb86fc8a5d39daf65eae410c002e2c
wisdom <miabiyewisdom@gmail.com>
2021-11-04 19:54:51 -0700

    inifnite scroll pushed

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ const count = 10;
 const apiKey = "bp4Dz-0hwgq2NSGhs9M0w9sER0aLX2ni1AxmzDzcSRo";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 let photoArray = [];
+let imagesLoaded = 0;
+let totalImages = 0;
+let ready = false;
+
 //get photos from Unslpash API
 console.log(apiUrl);
-async function getPhotos(){
+const getPhotos = async () => {
 
     try {
 
         const response = await fetch(apiUrl);
         photoArray = await response.json();
         console.log(photoArray);
+        return photoArray;
         
     } catch (error) {
         
@@ -24,6 +29,8 @@ function displayPhotos()
     photoArray.forEach((photo) => {
         //create an anchor
         const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html)
+        item.setAttribute('target' , '_blank');
     })
 }
 getPhotos();
